0bf88e6ef5bcefd2479d532386198edd380589d7
HassanAbass <subscriber@LAPTOP-3S8A8JCD.localdomain>
2022-08-23 18:57:46 +0200

    remove dist files

--- dist/libs/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.THUMB_DIR = exports.IMAGE_DIR = void 0;
-exports.IMAGE_DIR = "images";
-exports.THUMB_DIR = "thumbs";
