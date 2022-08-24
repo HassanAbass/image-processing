91613375c9dd0d18a150cdf0adc462cb5560fe6e
HassanAbass <subscriber@LAPTOP-3S8A8JCD.localdomain>
2022-08-24 09:53:44 +0200

    append status on response

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { VALIDATION_CODE } from "../libs/constants";
+import { SUCCESS_CODE, VALIDATION_CODE } from "../libs/constants";
 import {
     fileExists,
     resizedName,
@@ -12,19 +12,21 @@ const router = express.Router();
 router.get(
     "/images",
     notFound,
-    async (req: express.Request, res: express.Response): Promise<void> => {
+    async (req: express.Request, res: express.Response) => {
         const width: number = parseInt(req.query.width as string);
         const height: number = parseInt(req.query.height as string);
         const fileName: string = req.query.fileName as string;
         try {
             if (fileExists(thumbPath(resizedName(fileName, width, height))))
-                return res.sendFile(
-                    thumbPath(resizedName(fileName, width, height))
-                );
+                return res
+                    .status(SUCCESS_CODE)
+                    .sendFile(thumbPath(resizedName(fileName, width, height)));
             await writeThumbFile(fileName, width, height);
-            res.sendFile(thumbPath(resizedName(fileName, width, height)));
+            return res
+                .status(SUCCESS_CODE)
+                .sendFile(thumbPath(resizedName(fileName, width, height)));
         } catch (e) {
-            res.status(VALIDATION_CODE).send(e);
+            return res.status(VALIDATION_CODE).send(e);
         }
     }
 );
